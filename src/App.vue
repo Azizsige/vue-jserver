501bb655ebfcdfac2f2d2d0ff3dd3cfb35045016
azizsige <kurniawan00azizsige@gmail.com>
2022-10-18 13:21:57 +0700

    first commit: integrate user api sign in

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <SignUp />
+  </div>
+</template>
+<script>
+// import SignUp
+import SignUp from "./components/SignUp.vue";
+export default {
+  components: {
+    SignUp,
+  },
+};
+</script>
+<style lang=""></style>
---
+++ src/components/SignUp.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <img class="logo" src="./../assets/vue.svg" alt="" srcset="" />
+    <h1>Sign Up</h1>
+    <div class="register">
+      <input v-model="name" type="text" placeholder="Enter Name" />
+      <input v-model="email" type="email" placeholder="Enter Email" />
+      <input v-model="password" type="password" placeholder="Enter Password" />
+      <button @click="signUp" type="submit">Sign Up</button>
+    </div>
+  </div>
+</template>
+<script>
+import axios from "axios";
+export default {
+  name: "SignUp",
+  data() {
+    return {
+      name: "",
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    async signUp() {
+      let results = await axios.post("http://localhost:3000/user", {
+        email: this.email,
+        password: this.password,
+        name: this.name,
+      });
+      console.warn(results);
+      if ((results.status = 201)) {
+        alert("signup");
+        localStorage.setItem("user-info", JSON.stringify(results.data));
+      }
+    },
+  },
+};
+</script>
+<style>
+.logo {
+  width: 100px;
+}
+
+.register input {
+  width: 300px;
+  height: 40px;
+  padding-left: 20px;
+  display: block;
+  margin: 0 auto 30px auto;
+  border: 1px solid skyblue;
+}
+
+.register button {
+  width: 320px;
+  height: 40px;
+  border: 1px solid skyblue;
+  background-color: skyblue;
+  color: #fff;
+  cursor: pointer;
+}
+</style>
