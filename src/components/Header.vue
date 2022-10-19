83992f5fef4b87cdf8aad88e54c70f122f38bbd1
azizsige <kurniawan00azizsige@gmail.com>
2022-10-19 13:09:43 +0700

    add: Header component

---
+++ src/components/Header.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container-list">
+    <ul class="list">
+      <li><a href="">Home</a></li>
+      <li><a href="">Add Restaurant</a></li>
+      <li><a href="">Update Restaurant</a></li>
+      <li><a href="">Logout</a></li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: "Header",
+};
+</script>
+<style>
+.container-list {
+  background-color: #f1f1f1;
+  width: 100%;
+  padding: 10px;
+}
+
+.list {
+  list-style: none;
+  display: flex;
+  justify-content: space-around;
+  padding: 0;
+  margin: 0;
+}
+
+.list li {
+  padding: 10px;
+}
+
+.list li a {
+  text-decoration: none;
+  color: #000;
+}
+
+.list li a:hover {
+  color: #f00;
+}
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
+    <Header />
     <h1>Hello User, Welcome Homepage</h1>
   </div>
 </template>
 <script>
+import Header from "./Header.vue";
 export default {
   name: "Home",
+  components: {
+    Header,
+  },
   mounted() {
     let users = localStorage.getItem("user-info");
     if (!users) {
